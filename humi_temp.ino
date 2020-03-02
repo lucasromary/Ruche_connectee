38c9c5b2347c456dbe6a0e047ada938f81157430
lucasromary <44670154+lucasromary@users.noreply.github.com>
2020-03-02 14:24:12 +0100

    Add files via upload
    Test lecture humi&temp DHT22

---
+++ humi_temp.ino
@@ -0,0 +1,67 @@
+// ---------------------------------------------------------------------
+// This file is a example for use the library armapi with
+// ARM NANO (NANO_LP with sigfox) on arduino board.
+//
+// This software send periodically 'Hello Sigfox' to Sigfox network.
+//
+// Created date: 13.02.2016
+// ---------------------------------------------------------------------
+
+/***********************************************************************
+
+ Copyright (c) 2016 ATIM
+
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+***********************************************************************/
+
+// ---------------------------------------------------------------------
+// Include
+// ---------------------------------------------------------------------
+
+#include "DHT.h"
+#define DHTPIN 4     // what pin we're connected to
+
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+float SensorTemperature = 0;
+int SensorHumidity = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{  
+
+int humidity = dht.readHumidity();
+Serial.print("humidité : ");
+Serial.println(humidity);
+float Temperature = dht.readTemperature();
+Serial.print("température : ");
+Serial.println(Temperature);
+delay(3000);
+}
